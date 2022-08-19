5b84993ff1bb776e563b5312adb39eae6f75f1bc
mita <cpjvmm@outlook.jp>
2022-08-19 14:49:51 +0900

    fix: 修正誤りを修正

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,6 +3,7 @@
 class Store < ApplicationRecord
   validates :ieul_store_id, presence: true
   validates :address, presence: true
+  validates :introduction_text, presence: true
   validates :catch_copy, presence: true
   has_many :valuation_areas, dependent: :restrict_with_error
   # TODO: 口コミができたら口コミをhas_manyに足す
