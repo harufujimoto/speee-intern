2dfbc702d38a00e7d75d5e57077ec05a747cadd4
Yamamoto Tatsuya <dasixi34@gmail.com>
2022-08-22 20:49:32 +0900

    fix: N+1問題に対応

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,6 +3,7 @@
 class StoresController < ApplicationController # rubocop:disable Style/Documentation
   def show
     @store = Store.find(params[:id])
+    @reviews = @store.reviews.includes(:city)
     @satisfaction_average = Review.average(:satisfaction)
   end
 end
