24fcaaa6e1c0d5e911eea032718915dc6f1f4f2f
Yamamoto Tatsuya <dasixi34@gmail.com>
2022-08-23 10:39:52 +0900

    fix: 再度N+1問題に対応

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-class StoresController < ApplicationController # rubocop:disable Style/Documentation
+class StoresController < ApplicationController
   def show
-    @store = Store.find(params[:id])
-    @reviews = @store.reviews.includes(:city)
+    @store = Store.includes(:company, reviews: { city: :prefecture }, city: :prefecture).find(params[:id])
     @satisfaction_average = Review.average(:satisfaction)
   end
 end
