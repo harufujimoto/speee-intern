fc2ed7316c7c07f69bb8f99f1f8ef8356448efd2
Yamamoto Tatsuya <dasixi34@gmail.com>
2022-08-24 17:20:19 +0900

    add: 市区町村リストの表示とリンク

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class StoresController < ApplicationController
+  def index
+    @city = City.includes(:prefecture).find(params[:city])
+    @stores = Store.includes(:reviews, city: :prefecture).where(city: @city)
+    @satisfaction_averages = Review.where(store: @stores).group(:store).average(:satisfaction)
+    @cities = City.where(prefecture: @city.prefecture)
+  end
+
   def show
     @store = Store.includes(:company, reviews: { city: :prefecture }, city: :prefecture).find(params[:id])
     @satisfaction_average = Review.average(:satisfaction)
