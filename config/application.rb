cbd2618b152e8e3ab757ddb29296a1e8ed951162
Shuma-Yamamoto <ebishuumai0743@gmail.com>
2022-08-25 15:41:21 +0900

    fix: rubocopを通しました

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module HrEngInternship20221stTeam2
     config.load_defaults 7.0
 
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
 
     # Configuration for the application, engines, and railties goes here.
     #
