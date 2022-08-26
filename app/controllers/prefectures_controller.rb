336123aa70c6d92430bf2998c645ef8bff53ac63
masaki.mita <cpjvmm@outlook.jp>
2022-08-26 11:44:41 +0900

    add: トップページと市区町村一覧ページを追加

---
+++ app/controllers/prefectures_controller.rb
@@ -0,0 +1,9 @@
+class PrefecturesController < ApplicationController
+  def index
+    @prefectures = Prefecture.all
+  end
+
+  def show
+    @cities = City.includes(:prefecture).where(prefectures: params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   resources :stores, only: %i[index show]
   resources :evaluation_requests, only: %i[new create]
   resources :reviews, only: :show
+  resources :prefectures, only: %i[index show]
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "prefectures#index"
 end
