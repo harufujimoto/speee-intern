e0ed6c956678d0bc00ac9cfd693fdc27c04f37a0
H.Fujimoto <harufujimoto.311@gmail.com>
2022-08-26 18:45:49 +0900

    add: kaminariページネーションをstoreページに追加

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -12,5 +12,6 @@ class StoresController < ApplicationController
     @store = Store.includes(:company, reviews: { city: :prefecture }, city: :prefecture).find(params[:id])
     @company = @store.company
     @satisfaction_average = Review.where(store: @store).average(:satisfaction)
+    @review = @store.reviews.page(params[:page]).per(1)
   end
 end
