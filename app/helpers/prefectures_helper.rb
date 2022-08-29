56e30de42fa25595f48e1fcb591d88da46508e2d
H.Fujimoto <harufujimoto.311@gmail.com>
2022-08-29 19:36:41 +0900

    fix: linter対応

--- app/helpers/prefectures_helper.rb
+++ app/helpers/prefectures_helper.rb
@@ -2,6 +2,6 @@
 
 module PrefecturesHelper
   def exist_store(pref)
-    Store.eager_load(city: :prefecture).where(city: {prefecture: pref.id}).present?
+    Store.eager_load(city: :prefecture).where(city: { prefecture: pref.id }).present?
   end
 end
