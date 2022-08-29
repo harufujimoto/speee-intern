e495d92b6154851702843d3f7119057c86411b3a
Yamamoto Tatsuya <dasixi34@gmail.com>
2022-08-29 13:13:41 +0900

    change: レビューを受けてURLを変更

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :company do
     name { 'あんず不動産' }
     ieul_company_id { 1 }
-    logo_url { 'https://foodslink.jp/syokuzaihyakka/syun/fruit/abricotto/anzu21s.jpg' }
+    logo_url { 'https://example.com' }
   end
 end
