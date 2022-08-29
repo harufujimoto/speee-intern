f2d4662910f02ce64159ad02ad99f80015c57d4e
H.Fujimoto <harufujimoto.311@gmail.com>
2022-08-29 14:51:07 +0900

    fix: りょうさん レビューに対応

--- app/helpers/prefectures_helper.rb
+++ app/helpers/prefectures_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module PrefecturesHelper
+  def exist_store(cities)
+    res = false
+    cities.each do |city|
+      res = true if city.stores.present?
+    end
+    res
+  end
 end
