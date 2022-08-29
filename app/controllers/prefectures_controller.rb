faf96a85436f2dae1d546351c9efec7e068df19d
H.Fujimoto <harufujimoto.311@gmail.com>
2022-08-29 19:50:52 +0900

    fix: 関数をcontrollerに移動

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -5,8 +5,15 @@ class PrefecturesController < ApplicationController
     @prefectures = Prefecture.all
   end
 
+  def exist_store
+    Store.eager_load(city: :prefecture).where(city: { prefecture: params[:id] }).present?
+  end
+
   def show
     @cities = City.where(prefecture: params[:id])
     @prefecture = Prefecture.find(params[:id])
+    @exist_store = Store.eager_load(city: :prefecture).where(city: { prefecture: params[:id] }).present?
   end
+
+  helper_method :exist_store
 end
--- app/helpers/prefectures_helper.rb
+++ app/helpers/prefectures_helper.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 module PrefecturesHelper
-  def exist_store(pref)
-    Store.eager_load(city: :prefecture).where(city: { prefecture: pref.id }).present?
-  end
+
 end
